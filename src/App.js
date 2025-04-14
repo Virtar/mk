4dbd65e596590194aae2d63b69c236b5db597546
Virginia <128263468+Virtar@users.noreply.github.com>
2025-04-14 16:28:44 -0300

    Create App.js

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import './App.css';
+import MortalKombatTournament from './components/MortalKombatTournament';
+
+function App() {
+  return (
+    <div className="App">
+      <MortalKombatTournament />
+    </div>
+  );
+}
+
+export default App;
